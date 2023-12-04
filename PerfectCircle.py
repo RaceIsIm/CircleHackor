c7eeb2df345499321b825a311fad4210357c1927
ida1337 <pykerust@gmail.com>
2023-12-05 00:42:14 +0300

    Good settings

--- PerfectCircle.py
+++ PerfectCircle.py
@@ -7,8 +7,8 @@ import time
 
 ready = True
 aim_key = 0x14 #https://learn.microsoft.com/en-us/windows/win32/inputdev/virtual-key-codes
-r = 330 #Radius of your circle
-circle_steps = 30 #Amount of moves to complete the circle, lower = faster
+r = 380 #Radius of your circle
+circle_steps = 25 #Corners? of your circle i guess?, lower = faster (2 linear 3 = triangle, 4 = square)
 width, height = pyautogui.size() #Screen size X , Y
 y_offset = (1 / 27) * height #Offset of the center white dot
 center_x, center_y = width / 2, (height - y_offset) / 2 #Center point
@@ -27,22 +27,22 @@ def IsKeyHeld(key):
 
 def Circle(radius, steps):
     pyautogui.mouseDown()
-    Sleep(10)
+    Sleep(5)
     for i in range(steps + 2): #+2 to fully finish the circle
         if IsKeyHeld(aim_key):  
             angle = i * (2.0 * np.pi / steps)
             dx = int(center_x + radius * np.cos(angle))
             dy = int(center_y + radius * np.sin(angle))
             pyautogui.moveTo(dx,dy)
-        pyautogui.mouseUp()
+    pyautogui.mouseUp()
 
 while True:
-    if IsKeyHeld(aim_key):
+    if win32api.GetAsyncKeyState(aim_key):
         if ready:
             pyautogui.moveTo((center_x+ r), center_y) #Move mouse to Beginning of circle
-            Sleep(10)
+            Sleep(5)
             Circle(r, circle_steps)
-            Sleep(1000)
             ready = False
+            Sleep(1000)
     if not IsKeyHeld(aim_key):
         ready = True
